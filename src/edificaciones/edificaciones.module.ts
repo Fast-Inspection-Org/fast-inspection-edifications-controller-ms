196bc84f617ce1a8679a9e5709538163325650d4
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-20 01:37:41 -0400

    feat(edificaciones-module): integrate the inspection service using TCP microservices

--- src/edificaciones/edificaciones.module.ts
+++ src/edificaciones/edificaciones.module.ts
@@ -3,9 +3,23 @@ import { EdificacionesService } from './edificaciones.service';
 import { EdificacionesController } from './edificaciones.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Edificacion } from './entities/edificacione.entity';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { NameInspectionsService } from 'src/utils/globals';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Edificacion])],
+  imports: [
+    TypeOrmModule.forFeature([Edificacion]),
+    ClientsModule.register([
+      {
+        name: NameInspectionsService,
+        transport: Transport.TCP,
+        options: {
+          host: process.env.INSPECTIONS_SERVICE_HOST,
+          port: parseInt(process.env.INSPECTIONS_SERVICE_PORT),
+        },
+      },
+    ]),
+  ],
   controllers: [EdificacionesController],
   providers: [EdificacionesService],
 })
---
+++ src/utils/globals.ts
@@ -0,0 +1,1 @@
+export const NameInspectionsService = "INSPECTIONS_SERVICE"
